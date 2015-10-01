241b50115de259096ee698f2f0683bfe96993c8a
Hans Schweisser <hans.schweisser@gmail.com>
2015-10-01 20:41:45 +0200

    Add proc compilation into L2 compiler

--- l2.pl
+++ l2.pl
@@ -2,22 +2,58 @@
 use Data::Dumper;
 use strict;
 
+
 my $file = $ARGV[0];
 open FILE, $file or die "cannot open file";
 my $output = join("", <FILE>);
 close FILE;
 
 my @outputcode;
-my @output;
 my @endstack;
+
+
+my @procnamespace;
+sub proc_push {
+    my ($entry) = @_;
+    my $last = $procnamespace[(scalar @procnamespace)-1];
+    push($last , $entry);
+}
+sub proc_newnamespace {
+    push(@procnamespace,[]);
+}
+sub proc_prevnamespace {
+    return pop(@procnamespace);
+}
+proc_newnamespace();
+
+
+my @codenamespace;
+sub code_push {
+    my ($entry) = @_;
+    my $last = $codenamespace[(scalar @codenamespace)-1];
+    push($last,$entry);
+}
+
+sub code_newnamespace {
+    push(@codenamespace,[]);
+}
+
+sub code_prevnamespace {
+    return pop(@codenamespace);
+}
+
+code_newnamespace();
 
 my @variables;
 my $var_index = 0;
 sub get_var_index{
     $var_index++;
-    return "var-". ($var_index-1);
+    return sprintf("label%04i" , ($var_index-1));
 }
 push(@variables,{name=>"zero",value=>"0x0",index=>"zero",});
+
+
+my @procedures;
 
 sub get_tmp_var{
     my ($val) = @_;
@@ -29,6 +65,44 @@ sub get_tmp_var{
 
 
 my @namespace = ();
+sub var_push {
+    my ($entry) = @_;
+    my $last = $namespace[(scalar @namespace)-1];
+    push($last,$entry);
+}
+sub var_newnamespace{
+    push(@namespace,[]);
+}
+sub var_prevnamespace{
+    return pop(@namespace);
+}
+
+var_newnamespace();
+
+
+
+
+sub get_index_of{
+    my ($db,$name) = @_;
+    my @dbl = @{$db};
+    for(my $i=(scalar @dbl)-1;$i>=0;$i--){
+	my @ns = @{$dbl[$i]};
+	foreach my $h (@ns){
+	    my %h = %$h;
+	    if( $h{name} eq $name ) {
+		return $h{index};
+	    }
+	}
+    }
+    die "Unknown  get_index_of $name\n";
+}
+
+
+
+
+
+
+
 sub get_index_of_variable{
     my ($name) = @_;
     for(my $i=(scalar @namespace)-1;$i>=0;$i--){
@@ -59,7 +133,6 @@ sub is_variable{
 
 my @lines = split(/\n/, $output);
 
-push ( @namespace, []);
 
 for(my $i=0;$i<(scalar @lines);++$i){
     my $line = $lines[$i];
@@ -86,7 +159,13 @@ for(my $i=0;$i<(scalar @lines);++$i){
 	my $end = pop(@endstack);
 	my %end = %{$end};
 	if( $end{type} eq "if" ){
-	    push(@output, {type=>"label", name=>$end{index},});
+	    code_push({type=>"label", name=>$end{index},});
+	}
+	if( $end{type} eq "proc" ){
+	    code_push({type=>"procend", index=>$end{index},name=>$end{index},});
+	    my @body = code_prevnamespace();
+	    proc_prevnamespace();
+	    push(@procedures, { code=> @body, head=>$end,});
 	}
     }elsif($cmd[0] eq "set" ){
 	if(! ($cmd[1] && $cmd[2]) ){
@@ -98,11 +177,11 @@ for(my $i=0;$i<(scalar @lines);++$i){
 	if( is_variable($cmd[2]) ){
 	    my $vi1 = get_index_of_variable($cmd[1]);
 	    my $vi2 = get_index_of_variable($cmd[2]);
-	    push(@output, {code=> "copy $vi2 $vi1",});
+	    code_push({code=> "copy $vi2 $vi1",});
 	}else{
 	    my $vi1 = get_index_of_variable($cmd[1]);
 	    my $tmpvar = get_tmp_var($cmd[2]);
-	    push(@output, {code=>"copy $tmpvar $vi1",});
+	    code_push({type=>"copy", arg1=>$tmpvar, arg2=> $vi1,});
 	}
     }elsif($cmd[0] eq "if"){
 	push(@namespace,[]);
@@ -111,18 +190,18 @@ for(my $i=0;$i<(scalar @lines);++$i){
 	    my $vi = get_index_of_variable($cmd[1]);
 	    my $tmp = get_tmp_var("0x0");    
 	    push(@endstack,{type=>"if", arg1=> $vi , arg2 => $tmp , index=>$ifindex,});
-	    push(@output, {type=>"if", arg1=> $vi, arg2 => $tmp, index=>$ifindex,});
+	    code_push( {type=>"if", arg1=> $vi, arg2 => $tmp, index=>$ifindex,});
 	}elsif( $cmd[1] && $cmd[2] ) {
 	    if( is_variable($cmd[2]) ){
 		my $vi1 = get_index_of_variable($cmd[1]);
 		my $vi2 = get_index_of_variable($cmd[2]);
-		push(@output, {type=>"if", arg1=>$vi1 , arg2 =>$vi2, index=>$ifindex,});
+		code_push( {type=>"if", arg1=>$vi1 , arg2 =>$vi2, index=>$ifindex,});
 		push(@endstack, {type=>"if", arg1=>$vi1 , arg2 =>$vi2, index=>$ifindex,});		
 	    }else{
 		my $vi1 = get_index_of_variable($cmd[1]);
 		my $tmp = get_tmp_var($cmd[2]);
 		push(@endstack,{type=>"if", arg1=> $vi1 , arg2 => $tmp , index=>$ifindex,});
-	        push(@output, {type=>"if", arg1=> $vi1, arg2 => $tmp, index=>$ifindex,});	
+	        code_push( {type=>"if", arg1=> $vi1, arg2 => $tmp, index=>$ifindex,});	
 	    }
 	}else{
 	    die "compilation error: not recognised 'if' statement";
@@ -134,13 +213,34 @@ for(my $i=0;$i<(scalar @lines);++$i){
 	
 	my $end = pop(@endstack);
 	my %end = %{$end};
-print Dumper(\%end);
 	if( $end{type} eq "if" ){
-	    push(@output,{type=>"else", label=>$end{index},});
+	    code_push({type=>"else", label=>$end{index},});
 	}else{
 	    die "compilation error: expected 'if' before 'else'";
 	}
 	push(@endstack,$end);
+    }elsif($cmd[0] eq "proc"){
+	if(not $cmd[1]) {
+	    die "compilation error: no name of proc";
+	}
+	code_newnamespace();
+	var_newnamespace();
+	
+	my $procindex = get_var_index();
+	if( (scalar @cmd) == 2 ){
+	    proc_push({name=>$cmd[1], index=>$procindex,});
+	    proc_newnamespace();
+	    push(@endstack, {type=>"proc", index=>$procindex,name=>$cmd[1],});
+	    code_push( {type=>"prochead", index=>$procindex,name=>$cmd[1],});
+	}
+    }elsif($cmd[0] eq "call"){
+	if( not $cmd[1] ) {
+	    die "compilation error: call no procedure";
+	}
+	my $inx = get_index_of(\@procnamespace,$cmd[1]);
+	code_push({type=>"call", name=>$cmd[1], index=>$inx,});
+    }elsif($cmd[0] eq "idle"){
+	code_push({type=>"idle",});
     }else{
 	print "unknown line '$line'\n";
     }
@@ -151,39 +251,87 @@ foreach my $var (@variables){
     my %var = %{$var};
     my $index = $var{index};
     my $value = $var{value};
-    push(@output, {code=>"$index: db $value",});
+    code_push( {code=> [ "$index: db $value"] ,});
 }
 
-foreach my $var (@output) {
+
+
+sub generate_code {
+my ($list) = @_;
+my @list = @{$list};
+foreach my $var (@list) {
     if( ! $var->{code} ){
 	if( $var->{type} eq "label" ){
-	    $var->{code} = "$var->{name}: idle";
+	    $var->{code} =  ["$var->{name}: idle"];
 	}elsif ($var->{type} eq "if" ){
-	    $var->{code} = "ifjmp $var->{arg1} $var->{arg2} $var->{index}";
+	    $var->{code} = ["ifjmp $var->{arg1} $var->{arg2} $var->{index}"];
+	}elsif ($var->{type} eq "idle" ){
+	    $var->{code} = [ "idle" ];
+	}elsif ($var->{type} eq "copy" ){
+	    $var->{code} = [ "copy $var->{arg1} $var->{arg2}" ];
 	}elsif ($var->{type} eq "else" ){
-	    $var->{code} = "ifjmp zero zero $var->{label}";
+	    $var->{code} = ["ifjmp zero zero $var->{label}"];
+	}elsif ($var->{type} eq "call" ){
+	    my $indx = $var->{index};
+	    $var->{code} = ["copy return-var-$indx proc-return-$indx " ,
+			    "ifjmp zero zero $indx",	
+			    "return-var-$indx: db call-return-$indx", 
+			    "call-return-$indx: idle"  ];
+	}elsif($var->{type} eq "procend" ){
+	    $var->{code} = [ "ifjmp2 zero zero proc-return-$var->{index}" ];
+	}elsif($var->{type} eq "prochead" ) {
+	    $var->{code} = [	"proc-return-$var->{index}: db 0x0",
+				"$var->{index}: idle"];
 	}else{
-	    die "compilation error: unknown line type $var->{type}";
+	    die "compilation error: unknown line type '$var->{type}'";
 	}	
     }	
 }
-
-my @code ;
-my $code;
-foreach my $var (@output) {
-    push(@code,$var->{code});
-    $code .= "$var->{code}\n";
+return @list;
 }
-#print $code;
+
+my $maincode = code_prevnamespace();
+my @code = generate_code($maincode);
 
 
+my @maincode;
+my $code;
+foreach my $var (@code) {
+    push(@maincode,$var->{code});
+}
 
-print Dumper(\@variables);
-#print Dumper(\@namespace);
+my @proccode;
+
+foreach my $f (@procedures) {
+
+    generate_code($f->{code});
+    foreach my $l ( @{$f->{code}}){
+	push(@proccode,$l->{code});
+    }
+}
+
+my @fullcode;
+foreach my $l (@maincode){
+    push(@fullcode,@{$l});
+}
+foreach my $l (@proccode){
+    push(@fullcode,@{$l});
+}
+
+foreach my $l (@fullcode){
+    $code .= $l . "\n";
+}
+
+#print "\@fullcode = " . Dumper(\@fullcode);
+
+#print "\@maincode = " . Dumper(\@maincode);
+#print "\@proccode = " . Dumper(\@proccode);
 
 
-print Dumper(\@output);
-
+#print "\@variables = " . Dumper(\@variables);
+#print "\@procecures = " .  Dumper(\@procedures);
+#print "\@maincode = " . Dumper(\@maincode);
+#print "\@code = " . Dumper(\@code);
 
 sub convert_to_hex()
 {
